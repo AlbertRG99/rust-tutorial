343b0679ce9ed3a121af298059ebdb1040dc7306
AlbertRG99 <josealberto99@correo.ugr.es>
2026-07-11 16:27:00 +0200

    declaración de variables

---
+++ pildorasinformaticas/fundamentos/target/debug/deps/fundamentos-a281af9dce306cb3.d
@@ -0,0 +1,5 @@
+/home/albert/github/rust-tutorial/pildorasinformaticas/fundamentos/target/debug/deps/fundamentos-a281af9dce306cb3.d: src/main.rs
+
+/home/albert/github/rust-tutorial/pildorasinformaticas/fundamentos/target/debug/deps/fundamentos-a281af9dce306cb3: src/main.rs
+
+src/main.rs:
---
+++ pildorasinformaticas/fundamentos/target/debug/fundamentos.d
@@ -0,0 +1,1 @@
+/home/albert/github/rust-tutorial/pildorasinformaticas/fundamentos/target/debug/fundamentos: /home/albert/github/rust-tutorial/pildorasinformaticas/fundamentos/src/main.rs
